bcfb8ddfd7e2a40a7dfeb8bc76fb61daa1b8550a
Addison Elliott <addison.elliott@gmail.com>
2018-12-05 18:18:00 -0800

    Went through and began commenting code
    Not sure if my syntax follows what has been done before but I think it's helpful and I want to take a stab at it

--- bin/mongodb-runner.js
+++ bin/mongodb-runner.js
@@ -1,21 +1,28 @@
 #!/usr/bin/env node
 
-var path = require('path');
-var fs = require('fs');
-var run = require('../');
-var clui = require('clui');
-var debug = require('debug')('mongodb-runner:bin:mongodb-runner.js');
+const path = require('path');
+const fs = require('fs');
+const run = require('../');
+const clui = require('clui');
+const debug = require('debug')('mongodb-runner:bin:mongodb-runner.js');
 
+// Process arguments, specifying that debug option is a boolean
 var args = require('minimist')(process.argv.slice(2), {
   boolean: ['debug']
 });
 
+// Set debug env variable so debug commands are printed out
 if (args.debug) {
   process.env.DEBUG = 'mongodb-runner*';
 }
 
+// Update action to be the specified action, first command given or start by default
 args.action = args.action || args._[0] || 'start';
 
+// Delete all unnamed arguments since they are not used
+delete args._;
+
+// Display help text and quit
 if (args.help || args.h) {
   /* eslint no-sync:0 no-console:0 */
   var usage = fs.readFileSync(path.resolve(__dirname, '../usage.txt')).toString();
@@ -24,6 +31,7 @@ if (args.help || args.h) {
   process.exit(1);
 }
 
+// Display version from package.json and quit
 if (args.version) {
   var pkg = require('../package.json');
 
@@ -33,6 +41,7 @@ if (args.version) {
 
 debug('Running action `%s` with args %s...', args.action, args);
 
+// Display spinner if not running this in continuous integration
 if (!process.env.CI) {
   if (args.action === 'start') {
     new clui.Spinner('Starting a MongoDB deployment to test against...').start();
@@ -41,6 +50,7 @@ if (!process.env.CI) {
   }
 }
 
+// Run with given arguments now
 run(args, function(err) {
   if (err) {
     console.error(err);
